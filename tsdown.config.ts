5a3355d708a2b9a5a3ed3967c07294ecfc7a1256
Greg Richardson <greg.nmr@gmail.com>
2026-06-24 23:16:10 -0600

    feat: cjs support

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'tsdown'
 export default defineConfig({
   entry: ['src/index.ts', 'src/jsx-runtime.ts', 'src/jsx-dev-runtime.ts'],
   outDir: 'dist',
-  format: 'esm',
+  format: ['esm', 'cjs'],
   fixedExtension: false,
   clean: true,
   dts: true,
